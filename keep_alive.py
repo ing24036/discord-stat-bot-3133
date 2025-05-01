91ba5c0b2c39ff2d04bc8cb5e8b60f6e5b249ff8
Phanuphat <ingcho007@gmail.com>
2025-05-01 19:30:33 +0700

    update data excel 2

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "✅ Bot is alive!"
+    return "✅ Bot is alive!", 200  # ✅ เพิ่ม status code 200
 
 def run():
     app.run(host='0.0.0.0', port=8080)
